2e441d060cb3561cbb839cda48bc22623d028de3
İbrahim Ayhan <ihalilayhan1@gmail.com>
2026-07-11 16:43:36 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/infina/price_simulator/PriceSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.infina.price_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infina/price_simulator/PriceSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infina.price_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
